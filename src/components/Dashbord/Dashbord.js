801e6a0e4acb55ac36103461cb8556ff9479406f
Bire11 <birhanuwoeldmariam@gmail.com>
2023-06-21 07:47:47 -0700

    co

--- src/components/Dashbord/Dashbord.js
+++ src/components/Dashbord/Dashbord.js
@@ -6,6 +6,7 @@ import ViewApp from './ViewApp';
 import ApplicationForm from './ApplicationForm';
 import EditApplicant from './EditApplicant';
 import AddCourse from './AddCourse';
+import GetDetail from './GetDetail';
 function Dashboard(){
 
     return (
@@ -17,6 +18,7 @@ function Dashboard(){
                     <Route path='/new-applicant' element={<ApplicationForm/>}/>
                     <Route path='/edit-applicant/:id' element={<EditApplicant/>}/>
                     <Route path='/addCourse/:id' element={<AddCourse/>}/>
+                    <Route path='/getDetail/:id' element={<GetDetail/>}/>
                 </Routes>
             </div>
         </Router>
